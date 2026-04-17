3815b5050a4963fbfcf0315cb48b4a8db5a98612
muhamadirpan266 <fotopribadi56@gmail.com>
2026-04-17 10:08:09 +0700

    update 3

--- break&continue.py
+++ break&continue.py
@@ -2,15 +2,11 @@ whislist_gunung = ["rinjani", "merbabu", "prau", "gede", "pangrango", "lawu", "c
 
 
 i = 0
-nama = whislist_gunung[i]
 
-while nama != "sumbing":
-    print(nama)
-    i += 1
-    nama = whislist_gunung[i]
-
-
-i = 0
-while i < 200000000:
-    print(f"{i+1}. saya suka mendaki gunung")
-    i = i+1
+while i < len(whislist_gunung):
+    if  (whislist_gunung[i] == "krinci"):
+        print("krinci sudah ketemu")
+        break
+    print(whislist_gunung[i])
+    i+=1
+    
